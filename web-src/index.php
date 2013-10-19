2b89c0e2295ed749875d90485e3e0eacc9f07d23
Ryan Tanner <mithranor@gmail.com>
2013-10-19 11:04:52 -0500

    moved index php

--- web-src/index.php
+++ web-src/index.php
@@ -5,7 +5,7 @@ Guy Cockrum
 Nick Morris
 Cameron Keith
 Chris Linstromberg 
-  Custom Cupcakes -->
+  B# -->
 
 
 <?php
@@ -48,107 +48,99 @@ Chris Linstromberg
     </form>
 
     <div class="page">
-    <img src="artwork/cupcake_icon.png" id = "logo" alt = "Custom Cupcake Logo">
-
-    <div class="about site">    
-        <p>Great Flavors!</p>
-        <p>Awesome Cupcakes</p>
-        <p>Fast Delivery</p>
-    </div>
 
 
+        <form id="register" method = "post" action = "addUser.php">
+            <header>Create a B# Account</header>
+            <div class="hr"><hr /></div>
+        <ul>
+            <li>
+                <input type="radio" name="mailingList" value="1"> Yes
+            </li>
+            <li>
+                <input type="text" placeholder="First Name" id="firstName" name = "firstName"required/>
+                <input type="text" placeholder="Last Name" id="lastName" name = "lastName"required/>
+            </li>
 
-    <form id="register" method = "post" action = "addUser.php">
-        <header>Create a Custome Cupcake Account</header>
-        <div class="hr"><hr /></div>
-    <ul>
-        <li>
-            <input type="radio" name="mailingList" value="1"> Yes
-        </li>
-        <li>
-            <input type="text" placeholder="First Name" id="firstName" name = "firstName"required/>
-            <input type="text" placeholder="Last Name" id="lastName" name = "lastName"required/>
-        </li>
+            <li>
+                <input type="email" placeholder="Email" id="email" name = "email"oninvalid="setCustomValidity('Please eneter a valid email address')" onchange="try{setCustomValidity('')}catch(e){}" required/>
+            </li>
 
-        <li>
-            <input type="email" placeholder="Email" id="email" name = "email"oninvalid="setCustomValidity('Please eneter a valid email address')" onchange="try{setCustomValidity('')}catch(e){}" required/>
-        </li>
+            <li>
+                <input type="password" id="password" name = "password" pattern = "[a-zA-Z0-9:.,?!@]{8,}" placeholder = "Password" oninvalid="setCustomValidity('Password must be greater than 8 characters')" onchange="try{setCustomValidity('')}catch(e){}" required/>
+            </li>
 
-        <li>
-            <input type="password" id="password" name = "password" pattern = "[a-zA-Z0-9:.,?!@]{8,}" placeholder = "Password" oninvalid="setCustomValidity('Password must be greater than 8 characters')" onchange="try{setCustomValidity('')}catch(e){}" required/>
-        </li>
+            <li>
+                <input type="text" placeholder="Telephone Number" id="phone" name = "phone" pattern = "[0-9]{10}" oninvalid="setCustomValidity('Phone number must be 10 characters, only numbers')" onchange="try{setCustomValidity('')}catch(e){}" required/>
+            </li>
 
-        <li>
-            <input type="text" placeholder="Telephone Number" id="phone" name = "phone" pattern = "[0-9]{10}" oninvalid="setCustomValidity('Phone number must be 10 characters, only numbers')" onchange="try{setCustomValidity('')}catch(e){}" required/>
-        </li>
+            <li>
+                <input type="text" placeholder="Address" id="address" name = "address"required/>
+            </li>
+                <input type="text" placeholder="City" id="city" name = "city" required/>
 
-        <li>
-            <input type="text" placeholder="Address" id="address" name = "address"required/>
-        </li>
-            <input type="text" placeholder="City" id="city" name = "city" required/>
+            <li>
+                <select name="state" id="state">
+                    <option value="" selected>-- Select One --</option>
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                </select>
+            </li>
 
-        <li>
-            <select name="state" id="state">
-    <option value="" selected>-- Select One --</option>
-    <option value="AL">Alabama</option>
-    <option value="AK">Alaska</option>
-    <option value="AZ">Arizona</option>
-    <option value="AR">Arkansas</option>
-    <option value="CA">California</option>
-    <option value="CO">Colorado</option>
-    <option value="CT">Connecticut</option>
-    <option value="DE">Delaware</option>
-    <option value="FL">Florida</option>
-<option value="GA">Georgia</option>
-<option value="HI">Hawaii</option>
-<option value="ID">Idaho</option>
-<option value="IL">Illinois</option>
-<option value="IN">Indiana</option>
-<option value="IA">Iowa</option>
-<option value="KS">Kansas</option>
-<option value="KY">Kentucky</option>
-<option value="LA">Louisiana</option>
-<option value="ME">Maine</option>
-<option value="MD">Maryland</option>
-<option value="MA">Massachusetts</option>
-<option value="MI">Michigan</option>
-<option value="MN">Minnesota</option>
-<option value="MS">Mississippi</option>
-<option value="MO">Missouri</option>
-<option value="MT">Montana</option>
-<option value="NE">Nebraska</option>
-<option value="NV">Nevada</option>
-<option value="NH">New Hampshire</option>
-<option value="NJ">New Jersey</option>
-<option value="NM">New Mexico</option>
-<option value="NY">New York</option>
-<option value="NC">North Carolina</option>
-<option value="ND">North Dakota</option>
-<option value="OH">Ohio</option>
-<option value="OK">Oklahoma</option>
-<option value="OR">Oregon</option>
-<option value="PA">Pennsylvania</option>
-<option value="RI">Rhode Island</option>
-<option value="SC">South Carolina</option>
-<option value="SD">South Dakota</option>
-<option value="TN">Tennessee</option>
-<option value="TX">Texas</option>
-<option value="UT">Utah</option>
-<option value="VT">Vermont</option>
-<option value="VA">Virginia</option>
-<option value="WA">Washington</option>
-<option value="WV">West Virginia</option>
-<option value="WI">Wisconsin</option>
-<option value="WY">Wyoming</option>
-</select>
-        </li>
-
-        <li>
-            <input type="text" placeholder="Zipcode" id="zip"  name = "zip" pattern = "[0-9]{5}" required/>
-        </li>
-    </ul>
-            <input type="submit" value="Sign Up" id="signUp" />
-  </form>
+            <li>
+                <input type="text" placeholder="Zipcode" id="zip"  name = "zip" pattern = "[0-9]{5}" required/>
+            </li>
+        </ul>
+                <input type="submit" value="Sign Up" id="signUp" />
+      </form>
 </div>
  
   </body>
