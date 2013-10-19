37d3639dabe47505f1f6e2c946d03938fd246168
gcockrum <gcockrum@smu.edu>
2013-10-19 11:29:17 -0500

    added band_page.php

---
+++ web-src/band_page.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Welcome!</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"> 
+  </head>
+
+  <body>
+  	<h1>Welcome to the band front page</h1>
+  	<img src="img/logo.PNG" id = "logo" alt = "Custom Cupcake Logo">
+  	<span id="navbar">
+  		<ul>
+  			<li>
+  				<a href="about">About Band</a>
+  			</li>
+  			<li>
+  				<a href="Pieces">Songs</a>
+  			</li>
+  			<li>
+  				<a href="management">Band Management</a>
+  			</li>
+  			<li>
+  				<a href="events">Upcoming Events</a>
+  			</li>
+  			<li>
+  				<a href="members">Band Members</a>
+  			</li>
+  			<li>
+  				<a href="contact">Contact Information</a>
+  			</li>
+  		</ul>
+  	</span>
+  	<div id="band management">
+  		<h2>My Bands</h2>
+  		<ul>
+  			<li>
+  			</li>
+  		</ul>
+  	</div>
+  </body>
