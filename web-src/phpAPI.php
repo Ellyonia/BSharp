819a8cd5badc53dba075c89cec3064112ea67108
mithranor <mithranor@gmail.com>
2013-10-22 22:27:37 -0500

    back to uname

--- web-src/phpAPI.php
+++ web-src/phpAPI.php
@@ -45,7 +45,7 @@ class phpAPI
 
 
 
-        $sql = "SELECT password, user_id FROM Users WHERE username = '$email'";
+        $sql = "SELECT password, user_id FROM Users WHERE username = '$username'";
 
         $result = mysql_query($sql) or die(mysql_error());
         while($row = mysql_fetch_assoc($result))
