d46311ab9e38cf029d5cfc2e02ee2c367e14d0c8
mithranor <mithranor@gmail.com>
2013-10-22 22:13:43 -0500

    added soem stuff for log in

---
+++ web-src/phpAPI.php
@@ -0,0 +1,72 @@
+<?
+
+class phpAPI
+{
+
+     
+    function phpAPI()
+    {
+        session_start();
+
+        $con = mysql_connect("localhost", "root", "narwhal");
+        if(!$con)
+            die('Could not connect: ' . mysql_error());
+
+        mysql_select_db("BSharp", $con)
+        or die("Unable to select database: " . mysql_error());
+    }
+
+
+    public function addUser()
+    {   
+        //add a user to the system
+        $user_id = $_POST['user_id'];
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $username = $_POST['email'];
+        $password = $_POST['pw'];
+
+        $query = "INSERT INTO Users(fname, lname, user_id, username,password) VALUES 
+            ('$fname','$lname','$user_id','$username','$password)";
+        if(!mysql_query($query))
+        {
+            return false;
+        }
+        else
+        {
+            return; 
+        }
+    
+    }
+
+
+    public function verifyUser()
+    {
+
+
+
+        $sql = "SELECT password, customer_ID FROM Customers WHERE email = '$email'";
+
+        $result = mysql_query($sql) or die(mysql_error());
+        while($row = mysql_fetch_assoc($result))
+        {
+            foreach($row as $cname => $cvalue)
+            {
+                if ($cname == "password")
+                    if ( $password == $cvalue)
+                        $valid = 1;
+
+              if ($cname == "user_ID")
+                $_SESSION['uID'] = $cvalue;
+            }
+        }
+        if ($valid == 1)
+            header("Location: band_page.php");
+        else
+            header("Location: index.php");
+        
+    }
+
+
+}
+?>
