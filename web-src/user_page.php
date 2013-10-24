a79d18761b94acc6b3aae96a0d7e14453b596393
Ryan Tanner <mithranor@gmail.com>
2013-10-24 15:14:58 -0500

    added value

--- web-src/user_page.php
+++ web-src/user_page.php
@@ -12,7 +12,7 @@
   		<h2>My Bands</h2>
   		<ul>
   			<li>
-          <input type="button" id="createBand" name="createBand">
+          <input type="button" id="createBand" name="createBand" value="Create Band"/>
   			</li>
   		</ul>
   </body>
