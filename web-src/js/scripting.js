97dcf4a7cbc15e40b8e37c26397103d80a2841f8
Ryan Tanner <mithranor@gmail.com>
2013-10-24 15:46:34 -0500

    now?

--- web-src/js/scripting.js
+++ web-src/js/scripting.js
@@ -27,7 +27,7 @@ function checkPass()
 function popUp()
 {
 
-	newwindow=window.open("http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/DB-GUI/web-src/BandCreation.php",'name',height=500,width=750');
+	newwindow=window.open("http://ec2-54-200-98-78.us-west-2.compute.amazonaws.com/DB-GUI/web-src/BandCreation.php",'name','height=500','width=750');
 	if (window.focus) {newwindow.focus()}
 		return false;
 	
