2bd6bd9734ea91427485df9b5f3d47fba23a83a0
Ryan Tanner <mithranor@gmail.com>
2013-10-24 15:11:28 -0500

    made user page

---
+++ web-src/user_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Welcome!</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"> 
+  </head>
+
+  <body>
+  	<h1>User Home</h1>
+
+  	<div class="band_pages">
+  		<h2>My Bands</h2>
+  		<ul>
+  			<li>
+  			</li>
+  		</ul>
+
+
+    <div class="rightSidebar">
+
+    </div>
+  </body>
