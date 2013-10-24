359a864467b6c5459c4511aa70f0a2438c6d9207
Ryan Tanner <mithranor@gmail.com>
2013-10-24 15:13:18 -0500

    button

--- web-src/user_page.php
+++ web-src/user_page.php
@@ -12,11 +12,7 @@
   		<h2>My Bands</h2>
   		<ul>
   			<li>
+          <form type="button" id="createBand" name="createBand">
   			</li>
   		</ul>
-
-
-    <div class="rightSidebar">
-
-    </div>
   </body>
