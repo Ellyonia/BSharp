64fc171fc5a318a26c2e436dcb777a2c22508dbd
Ryan Tanner <mithranor@gmail.com>
2013-10-24 14:29:52 -0500

    edit dimensions

--- web-src/BandCreation.php
+++ web-src/BandCreation.php
@@ -15,7 +15,7 @@
   		<ol>
   			<li>
   				About Band
-  				<input type='textArea' placeholder="Enter Information about your Band" id="aboutBand" name = "aboutBand" rows="50" cols="50" required/>
+  				<input type='textArea' rows="4" cols="50" placeholder="Enter Information about your Band" id="aboutBand" name = "aboutBand"  required/>
   			</li>
   		</ol>
 
