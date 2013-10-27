73b66252bd832293a7a86f120e21b7f61073727d
Robert Stewart <rcstewart@smu.edu>
2013-10-27 16:04:52 -0500

    asdfasd

--- web-src/js/bandPage.js
+++ web-src/js/bandPage.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 	$('#navbar li').click(function(e){
-		console.log(e.target);
+		//console.log(e.target);
 		//e.prevenDefault();
 		var makeVis = $(e.target).first().attr('href');
-		//var makeVis = $(e.target + ' a').attr('href');
 		console.log(makeVis);
 		$('.visible').addClass('hidden');
 		$('.visible').removeClass('visible');
 		//$('.selected').css({"background-color": "#181818"});
-		$.when($('.selected').removeClass('selected')).then(function(){
+		$.when($('.selected').removeClass('selected')).done(function(){
 		$(makeVis).addClass('visible');
 		$(e.target).addClass('selected');
 	});
