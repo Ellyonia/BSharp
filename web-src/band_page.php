d11f60cb92d188c73b2a0a4d9a6a10722328c303
gcockrum <gcockrum@smu.edu>
2013-10-27 10:36:43 -0500

    More html edits

--- web-src/band_page.php
+++ web-src/band_page.php
@@ -41,19 +41,23 @@
   		</ul>
   	</div>
   	<div id="about">
-      <h2>About</h2>
+      <h2>About Band</h2>
       <p>This is the greatest and best band in the world. All other bands are shit.</p>
   	</div>
   	<div id="pieces">
+      <h2>View Band Pieces</h2>
   	</div>
   	<div id="management">
-      <h2>Manager</h2>
+      <h2>Manage Band</h2>
       <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
   	</div>
   	<div id="events">
+      <h2>View Upcoming Events</h2>
   	</div>
   	<div id="members">
+      <h2>View/Edit Band Members</h2>
   	</div>
   	<div id="contact">
+      <h2>Contact Information</h2>
   	</div>
   </body>
