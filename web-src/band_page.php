73024c2d378f04d1f7fcd6b6e66dbf4a6824ab1a
Robert Stewart <rcstewart@smu.edu>
2013-10-27 11:17:04 -0500

    Adding hidden class to divs in band_page

--- web-src/band_page.php
+++ web-src/band_page.php
@@ -45,20 +45,20 @@
       <h2>About Band</h2>
       <p>This is the greatest and best band in the world. All other bands are shit.</p>
   	</div>
-  	<div id="pieces">
+  	<div id="pieces" class="hidden">
       <h2>View Band Pieces</h2>
   	</div>
-  	<div id="management">
+  	<div id="management" class="hidden">
       <h2>Manage Band</h2>
       <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
   	</div>
-  	<div id="events">
+  	<div id="events" class="hidden">
       <h2>View Upcoming Events</h2>
   	</div>
-  	<div id="members">
+  	<div id="members" class="hidden">
       <h2>View/Edit Band Members</h2>
   	</div>
-  	<div id="contact">
+  	<div id="contact" class="hidden">
       <h2>Contact Information</h2>
   	</div>
   </body>
