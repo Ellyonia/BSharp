10699998456c9b60ec36811e565c3ee324ac07d3
Robert Stewart <rcstewart@smu.edu>
2013-10-27 16:11:46 -0500

    debugging

--- web-src/js/bandPage.js
+++ web-src/js/bandPage.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 		//console.log(e.target);
 		//e.prevenDefault();
 		var makeVis = $(e.target).first().attr('href');
-		console.log(makeVis);
+		//console.log(makeVis);
 		$('.visible').addClass('hidden');
 		$('.visible').removeClass('visible');
-		//$('.selected').css({"background-color": "#181818"});
-		$.when($('.selected').removeClass('selected')).done(function(){
+		$.when($('.selected').css({"background-color": "#181818"})).done(function(){
+		$('.selected').removeClass('selected')
 		$(makeVis).addClass('visible');
 		$(e.target).addClass('selected');
 	});
