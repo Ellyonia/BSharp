4e5cdc6edbc1b19990857c334f7a0620b45e5419
Robert Stewart <rcstewart@smu.edu>
2013-10-27 12:33:45 -0500

    debug code

--- web-src/js/bandPage.js
+++ web-src/js/bandPage.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	$('#navbar li').click(function(e){
 		var makeVis = $(e.target + ' a').attr('href');
+		log(1);
 		$('visible').addClass('hidden');
+		log(2);
 		$('visible').removeClass('visible');
+		log(3);
 		$('#'+makeVis).addClass('visible');
+		log(4);
 	});
 
 
