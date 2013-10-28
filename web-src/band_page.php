d9ca6e9fb4cadd5dd48f81a71699d33f206fca8e
mithranor <mithranor@gmail.com>
2013-10-27 19:33:30 -0500

    added bands selection to band page

--- web-src/band_page.php
+++ web-src/band_page.php
@@ -56,6 +56,12 @@
   	<div class="band_pages">
   		<h2>My Bands</h2>
   		<ul>
+          <?php
+            include 'phpAPI.php';   
+
+            $phpInit = new phpAPI();
+            $phpInit->getBands();
+          ?>
   			<li>
   			</li>
   		</ul>
