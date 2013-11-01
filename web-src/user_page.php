6e3ae2b20aa6ffcdaa68eec294559be1cc6bfc96
gcockrum <gcockrum@smu.edu>
2013-11-01 15:30:55 -0500

    button

--- web-src/user_page.php
+++ web-src/user_page.php
@@ -31,7 +31,7 @@
             ?>
         </li>
   			<li>
-          <FORM METHOD="LINK" ACTION="BandCreation.php" id="userCreateBand">
+          <FORM METHOD="LINK" ACTION="BandCreation.php" id="createBand">
             <INPUT TYPE="submit" VALUE="Create Band">
           </FORM>
           <!--
