0a8650805a66741edfed72b9e9b899ceda9e430d
mithranor <mithranor@gmail.com>
2013-11-02 20:48:45 -0500

    add button

--- web-src/band_page.php
+++ web-src/band_page.php
@@ -829,6 +829,7 @@
 
       <form id = "editPhone" name = "editPhone" method = "post">
         <input type="text" placeholder="Telephone Number" id="phone" name = "phone" pattern = "[0-9]{10}" oninvalid="setCustomValidity('Phone number must be 10 characters, only numbers')" onchange="try{setCustomValidity('')}catch(e){}" required/>
+        <input type="submit" name="changePhone" id="changePhone" value="Change Phone Number"/>
       </form>
     </div>
 
