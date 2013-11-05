c8fe6e55a430982f6b8a2a34c2f5bc33f73262be
Ryan Tanner <mithranor@gmail.com>
2013-11-05 11:05:52 -0600

    fix

--- web-src/phpAPI.php
+++ web-src/phpAPI.php
@@ -26,7 +26,7 @@ class phpAPI
         $password = $_POST['newPassword'];
 
 
-        $count = "SELECT * from Users WHERE(username = '%s')", mysql_real_escape_string($email);
+        $count = "SELECT * from Users WHERE(username = '" . mysql_real_escape_string($email) . "')";
         
 
         if(!mysql_query($count))
@@ -45,11 +45,7 @@ class phpAPI
             else
             {
                 $query = "INSERT INTO Users(fname, lname, username,password) VALUES 
-                    ('%s', '%s','%s','%s')",
-                    mysql_real_escape_string($fname),
-                    mysql_real_escape_string($lname),
-                    mysql_real_escape_string($email),
-                    mysql_real_escape_string($password);
+                    ('" . mysql_real_escape_string($fname) . "', '" . mysql_real_escape_string($lname) . "','" . mysql_real_escape_string($email) . "','" . mysql_real_escape_string($password) . "')";
         /*
                 $query = "INSERT INTO Users(fname, lname, user_id, username,password) VALUES 
                     ('" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "',444,'" . $_POST['newEmail'] . "','" . $_POST['newPassword'] . "')";*/
@@ -60,9 +56,7 @@ class phpAPI
                 }
                 else
                 {
-                    $getID = mysql_query("SELECT user_id from Users where username = '%s' and password = '%s'", 
-                        mysql_real_escape_string($_POST['email']),
-                        mysql_real_escape_string($_POST['password']));
+                    $getID = mysql_query("SELECT user_id from Users where username = '" . mysql_real_escape_string($_POST['email']) . "' and password = '" . mysql_real_escape_string($_POST['password']) . "'");
 
                     $temp = mysql_fetch_assoc($getID);
                     $_SESSION['uid'] = $temp['user_id'];
