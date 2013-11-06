c3530f13bff098844676a31f5899d1b16183bc9d
Cameron Keith <ckeith@smu.edu>
2013-11-06 15:52:03 -0600

    Added several Sample PDFs to use for PDF display as well as sample code to display PDF in browser

---
+++ Sample_PDF_display/pdfDisplay.php
@@ -0,0 +1,9 @@
+<?php
+		echo  "hello";
+		$instrument = "Tenor_Sax";
+		$fileLocation = $instrument . ".pdf";
+		echo "<html>";
+		echo "<iframe src=$fileLocation width='1500' height='900'> ";
+		echo "</html>"
+
+?>
