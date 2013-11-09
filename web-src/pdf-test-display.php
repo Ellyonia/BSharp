ed63bf7502e06b3affcbd298a283bd30417bf63c
mithranor <mithranor@gmail.com>
2013-11-09 17:16:43 -0600

    retry

--- web-src/pdf-test-display.php
+++ web-src/pdf-test-display.php
@@ -1,8 +1,8 @@
 <?php
 
                 $instrument = "Piccolo";
-                //$fileLocation = "../Music/Mustang%20Band/Westen%20Peruna/" . $instrument . ".pdf";
-                $fileLocation = "Piccolo.pdf";
+                $fileLocation = "../Music/Mustang%20Band/Westen%20Peruna/" . $instrument . ".pdf";
+                //$fileLocation = "Piccolo.pdf";
                 echo "<html>";
                 echo "<iframe src=$fileLocation width='1500' height='900'> ";
                 echo "</html>"
