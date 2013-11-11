c2c30f733a34c2c695b88d9e83707fac5c2e0c45
mithranor <mithranor@gmail.com>
2013-11-10 19:52:55 -0600

    different test

--- web-src/upload_file.php
+++ web-src/upload_file.php
@@ -1,3 +1,11 @@
+<html lang="en">
+  <head>
+    <title>Name the files!</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src='js/bandPage.js'></script> 
+  </head>
+</html>
 <? 
 
 include 'phpAPI.php';
