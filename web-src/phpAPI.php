34484ed399dbf32fd86051a5dfa4ee80e01c9ccb
mithranor <mithranor@gmail.com>
2013-11-20 19:08:24 -0600

    test

--- web-src/phpAPI.php
+++ web-src/phpAPI.php
@@ -190,7 +190,7 @@ class phpAPI
             $newResult = mysql_query($newQ);
             $newTemp = mysql_fetch_assoc($newResult);
             $dir = $newTemp['directorFlag'];
-            //echo $dir;
+            echo $dir;
 
             if($dir == 0) {
                 echo "<li><img src='img/User-icon.png' class='isDirector'><a href='band_page.php?id=" . $temp['band_id'] . "'>$dir, $bName</a></li>";
