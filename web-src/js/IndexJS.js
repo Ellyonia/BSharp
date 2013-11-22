012a2cfbbc3d3f5e8143cf3ce70c386b10471327
Ryan Tanner <mithranor@gmail.com>
2013-11-22 16:51:59 -0600

    inline

--- web-src/js/IndexJS.js
+++ web-src/js/IndexJS.js
@@ -1,38 +1,6 @@
 $(document).ready(function(){
 	
-	window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '501087379989764',  // Development
-//  appId      : '551756141566063',  // Production
-    status     : true,
-    cookie     : true, 
-    xfbml      : true  
-  });
- 
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
-    if (response.status === 'connected') {
-      FB.api('/me', function(response) {
-        $.ajax({
-        type: "POST",
-        url: "checkFacebook.php",
-        data: { "email":response.email, "fname":response.first_name, "lname":response.last_name },
-        });
-      });
-    } else if (response.status === 'not_authorized') {
-      FB.login();
-    } else {
-      FB.login();
-    }
-  });
-  };
 
-  (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
   $('#signUp2').click(checkPass);
   
   });
