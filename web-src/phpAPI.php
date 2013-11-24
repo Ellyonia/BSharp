ad88a11906f3d00d8f8dcb03bb96c22b1f8bfe1a
Ryan Tanner <mithranor@gmail.com>
2013-11-24 14:27:43 -0600

    fix

--- web-src/phpAPI.php
+++ web-src/phpAPI.php
@@ -901,10 +901,7 @@ class phpAPI
 
         $fileLocation = "../Music/" . $bID . "/" . $pID . "/" . strtolower($instrument) . "_" . $part . ".pdf";
         
-        echo "$fileLocation";
-    
-
-
+        echo $fileLocation;
 
     }
 
