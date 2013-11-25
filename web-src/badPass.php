a7b567e8e44e71f3ce0808d39ee00c26dbc33e8d
Ryan Tanner <mithranor@gmail.com>
2013-11-25 13:33:01 -0600

    add baddpass.php

---
+++ web-src/badPass.php
@@ -0,0 +1,7 @@
+<HTML>
+	<body>
+		<header>
+			<h1>Password must be at least 8 characters, contain at least 1 uppercase letter, 1 lower case letter, and 1 number</h1>
+		</header>
+	</body>
+</HTML>
