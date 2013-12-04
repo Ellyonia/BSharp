6289f80f69f5fb002307650d7236ec73aa1443d2
Ryan Tanner <mithranor@gmail.com>
2013-12-04 16:10:03 -0600

    add style

--- web-src/badPass.php
+++ web-src/badPass.php
@@ -1,4 +1,10 @@
 <HTML>
+	<head>
+	    <title>Welcome to B#</title>
+	    <link rel="stylesheet" type="text/css" href="css/style.css"> 
+	    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	    <script type="text/javascript" src='js/IndexJS.js'></script>
+  	</head>
 	<body>
 		<header>
 			<a href='index.php'>Home</a>
