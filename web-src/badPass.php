734668c30959a3db0653bed738716d455c2fe3f6
Ryan Tanner <mithranor@gmail.com>
2013-12-04 16:07:12 -0600

    link to home page from badpass

--- web-src/badPass.php
+++ web-src/badPass.php
@@ -1,7 +1,8 @@
 <HTML>
 	<body>
 		<header>
-			<h1>Password must be at least 8 characters, contain at least 1 uppercase letter, 1 lower case letter, and 1 number</h1>
+			<h2>Password must be at least 8 characters, contain at least 1 uppercase letter, 1 lower case letter, and 1 number</h2>
+			<a href='index.php'><img src='img/wizard.png'height='15' width='15'>Home</a>
 		</header>
 	</body>
 </HTML>
