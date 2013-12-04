c6f139f87c4c78b9a2a72d3b2634b3f3e67f07f6
Ryan Tanner <mithranor@gmail.com>
2013-12-04 16:08:47 -0600

    add home button without wand

--- web-src/badPass.php
+++ web-src/badPass.php
@@ -1,8 +1,9 @@
 <HTML>
 	<body>
 		<header>
+			<a href='index.php'>Home</a>
 			<h2>Password must be at least 8 characters, contain at least 1 uppercase letter, 1 lower case letter, and 1 number</h2>
-			<a href='index.php'><img src='img/wizard.png'height='15' width='15'>Home</a>
+			
 		</header>
 	</body>
 </HTML>
