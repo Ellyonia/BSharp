149900d777c8bebe06b8982ff6fad97666c96cdb
Ellyonia <Ckeith@smu.edu>
2013-12-09 10:53:20 -0600

    Removing an sample file

--- Sample_PDF_display/pdfDisplay.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-		$instrument = "Piccolo";
-		$fileLocation = "../Music/Mustang%20Band/Westen%20Peruna/" . $instrument . ".pdf";
-		echo "<html>";
-		echo "<iframe src=$fileLocation width='1500' height='900'> ";
-		echo "</html>"
-
-?>
